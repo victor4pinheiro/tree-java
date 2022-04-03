98352cd8b7d309989029dc212c216b1d708c8383
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-04-02 23:14:29 -0300

    feat: add Nodo type

---
+++ src/Nodo.java
@@ -0,0 +1,13 @@
+public class Nodo {
+  int numero;
+  Nodo esquerda;
+  Nodo direita;
+  Nodo pai;
+
+  public Nodo() {
+    this.numero = 0;
+    this.esquerda = null;
+    this.direita = null;
+    this.pai = null;
+  }
+}
