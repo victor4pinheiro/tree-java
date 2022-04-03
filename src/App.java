4274324d4c614255f8c7629b6f00b179289adb88
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-04-02 23:20:34 -0300

    testing and learning trees with visual approach
    Signed-off-by: victor4pinheiro <victor.pinheiro.soft@gmail.com>

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) throws Exception {
+    Arvore arvore = new Arvore();
+
+    for (int i = 1; i < 10; i++) {
+      Random random = new Random();
+      Nodo tmp = new Nodo();
+      tmp.numero = random.nextInt();
+      arvore.inserir(tmp);
     }
+
+    try {
+      System.out.println("\n");
+      System.out.println(arvore.raiz.esquerda);
+      System.out.println(arvore.raiz.direita);
+      System.out.println("\n");
+    } catch (Exception e) {
+      System.out.println("Vazio");
+    }
+
+    arvore.listar(arvore.raiz);
+  }
 }
