c88a2159b59ee6e0d7d68f85791af4622e464f5e
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-04-12 11:19:36 -0300

    refactor: insert recursively on static raiz (root)

--- src/ArvoreView.java
+++ src/ArvoreView.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 
 public class ArvoreView {
 
-  public boolean inserirNumero(Nodo tmpNodo, Scanner tmpScanner) throws InputMismatchException {
+  public Boolean inserirNumero(Scanner tmpScanner) {
+    int numero = 0;
     try {
       System.out.println("Digite um número inteiro:");
-      tmpNodo.numero = tmpScanner.nextInt();
+      numero = tmpScanner.nextInt();
     } catch (InputMismatchException e) {
       System.out.println("Não é possível inserir valor não numérico");
       tmpScanner.nextLine();
       return false;
     }
+    Nodo.inserir(numero);
     return true;
   }
 }
--- src/Nodo.java
+++ src/Nodo.java
@@ -9,12 +9,48 @@ public class Nodo {
   int numero;
   Nodo esquerda;
   Nodo direita;
-  Nodo pai;
+  int nivelArvore;
+  public static Nodo raiz;
 
-  public Nodo() {
-    this.numero = 0;
+  /**
+   * Inicializa os valores no atual nodo
+   * 
+   * @param numero número a ser inserido no Nodo atual
+   */
+  public Nodo(int numero) {
+    this.numero = numero;
     this.esquerda = null;
     this.direita = null;
-    this.pai = null;
+  }
+
+  /**
+   * Insere valores na árvore a partir da raiz
+   * 
+   * @param numero número a ser adicionado
+   */
+  public static void inserir(int numero) {
+    raiz = inserirNodo(raiz, numero);
+  }
+
+  /**
+   * Insere o valor no Nodo de acordo com a sua posição atual na árvore de forma
+   * recursiva
+   * 
+   * @param tmpNodo Nodo atual a ser verificado e possivelmente atribuído
+   * @param value   valor a ser inserido no Nodo
+   * @return Nodo com o valor já atribuido
+   */
+  private static Nodo inserirNodo(Nodo tmpNodo, int value) {
+    if (tmpNodo == null)
+      return new Nodo(value);
+
+    if (value < tmpNodo.numero)
+      tmpNodo.esquerda = inserirNodo(tmpNodo.esquerda, value);
+    else if (value > tmpNodo.numero)
+      tmpNodo.direita = inserirNodo(tmpNodo.direita, value);
+    else
+      return tmpNodo;
+
+    return tmpNodo;
   }
 }
