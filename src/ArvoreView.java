ffe800e60fc11cd076579e2902ed8ea65c17ad82
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-04-12 12:53:10 -0300

    feat: list all nodos from arvore(tree) according to type of depth-first search

--- src/ArvoreView.java
+++ src/ArvoreView.java
@@ -2,7 +2,13 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ArvoreView {
-
+  /**
+   * Insere o nodo com o valor dentro da árvore
+   * 
+   * @param tmpScanner scanner temporário para entrada de valores digitadas pelo
+   *                   usuário
+   * @return verdadeiro se a inserção foi bem sucedidade, caso contrário, falso
+   */
   public Boolean inserirNumero(Scanner tmpScanner) {
     int numero = 0;
     try {
@@ -15,5 +21,54 @@ public class ArvoreView {
     }
     Nodo.inserir(numero);
     return true;
+  }
+
+  /**
+   * Lista os nodos de acordo com o tipo de ordem selecionada pelo usuário
+   * 
+   * @param tmpScanner scanner temporário para entrada de valores digitadas pelo
+   *                   usuário
+   */
+  public void listarNodos(Scanner tmpScanner) {
+    if (Nodo.raiz == null) {
+      System.out.println("Árvore vazia");
+      return;
+    }
+
+    int option = 0;
+    try {
+      System.out.println("Qual tipo de listagem em profundidade deseja fazer?");
+      System.out.println("1 - Pré-ordem (raiz - esquerda - direita)");
+      System.out.println("2 - Em ordem (esquerda - raiz - direita)");
+      System.out.println("3 - Pós-ordem (esquerda - direita - raiz)");
+      option = tmpScanner.nextInt();
+    } catch (InputMismatchException e) {
+      System.out.println("Valor não numérico");
+      return;
+    }
+
+    switch (option) {
+      case 1:
+        System.out.println("Pré-ordem:");
+        Nodo.preordem(Nodo.raiz);
+        System.out.print("\n");
+        break;
+
+      case 2:
+        System.out.println("Em ordem:");
+        Nodo.ordem(Nodo.raiz);
+        System.out.print("\n");
+        break;
+
+      case 3:
+        System.out.println("Pós-ordem:");
+        Nodo.posordem(Nodo.raiz);
+        System.out.print("\n");
+        break;
+
+      default:
+        System.out.println("Opção não reconhecida.");
+        break;
+    }
   }
 }
